7a515c2fd5d0f1073e3b5273f7ca87481d8a8523
wuzeyuresult <212602929@qq.com>
2021-01-29 16:36:10 +0800

    java的学习

---
+++ 初识java及数据类型/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+文档注释
+*/
+//行注释
+/*
+块注释
+*/
+
+//-encoding utf-8
+
+
+
+public class HelloWorld {
+	public static void main(String[] args){
+		int a;//未定义，无法使用
+		System.out.println(a);//println--打印并换行
+		//System.out.println(Integer.MAX_VALUE);//int最大值
+		//System.out.println(Integer.MIN_VALUE);//int最小值		
+		//System.out.println("HelloWorld");
+		//System.out.println(a);
+	}
+}
